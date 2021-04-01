e6f184996cd9125c53b3af00c6acd454921e70ed
vlle-dot <73817969+vlle-dot@users.noreply.github.com>
2021-04-01 12:27:57 +0300

    Add files via upload

--- js/clock.js
+++ js/clock.js
@@ -1,1 +1,12 @@
+var secondsForClock = setInterval(timeDisplay, 1000);
 
+function timeDisplay() {
+  var dt = new Date();
+  document.getElementById("datetime").innerHTML = 
+  (("0"+dt.getDate()).slice(-2)) +"."               //changes date format
+  + (("0"+(dt.getMonth()+1)).slice(-2)) +"."
+  + (dt.getFullYear()) +" "
+  + (("0"+dt.getHours()).slice(-2)) +":"
+  + (("0"+dt.getMinutes()).slice(-2)) + ":"
+  + (("0"+dt.getSeconds()).slice(-2));
+}
